07ec55003acab89b25a58e12e58ed4c847dd7b6e
meowmeowmeowmeow0513 <vivydiva2161@gmail.com>
2025-12-01 16:16:30 +0800

    Update AuthContext.tsx

--- AuthContext.tsx
+++ AuthContext.tsx
@@ -73,7 +73,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
 
   return (
     <AuthContext.Provider value={value}>
-      {!loading && children}
+      {children}
     </AuthContext.Provider>
   );
 };
