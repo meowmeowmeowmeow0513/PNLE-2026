391c9629d10f82f232192a3b30dce123daa88e51
meowmeowmeowmeow0513 <vivydiva2161@gmail.com>
2025-12-15 07:11:06 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'pnle-companion-v1';
+const urlsToCache = ['/', '/index.html'];
+
+// Install: Cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch: Serve from cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate: Clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) return caches.delete(cache);
+        })
+      );
+    })
+  );
+});
